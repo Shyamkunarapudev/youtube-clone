7928a25dd2cf94cf62a93520063d86a359b8cad0
shyamkunarapudev <shyamkumar14318@gmail.com>
2024-11-30 05:26:03 +0000

    Shifted SideBar and MainContainer into Body Component

---
+++ src/Body.jsx
@@ -0,0 +1,13 @@
+import Sidebar from './components/Sidebar'
+import MainContainer from './components/MainContainer'
+
+function Body () {
+  return (
+    <div className='flex'>
+      <Sidebar />
+      <MainContainer />
+  </div>
+  )
+}
+
+export default Body:
