43921e580f55036d613b177996c49883aa25ca4d
shyamkunarapudev <shyamkumar14318@gmail.com>
2024-12-02 11:49:19 +0000

    Created Routs to the childrens of Body component

--- src/App.jsx
+++ src/App.jsx
@@ -3,14 +3,35 @@ import './App.css'
 import Body from './components/Body'
 import Header from './components/Header'
 import appStore from './utils/appStore'
+import { createBrowserRouter, RouterProvider } from 'react-router-dom'
+import MainContainer from './components/MainContainer'
+import WatchPage from './components/WatchPage'
+
+const appRouter = createBrowserRouter([
+  {
+    path: "/",
+    element: <Body />,
+    children: [
+      {
+        path: "/",
+        element: <MainContainer />
+      },
+      {
+        path: "watch",
+        element: <WatchPage />
+      }
+    ]
+  }
+])
 
 function App() {
-
   return (
     <Provider store={appStore} >
       <div className='mx-4'>
         <Header />
-        <Body />
+        <RouterProvider router={appRouter}>
+          <Body />
+        </RouterProvider>
       </div>
     </Provider>
   )
---
+++ src/components/WatchPage.jsx
@@ -0,0 +1,9 @@
+function WatchPage () {
+  return (
+    <div>
+      <h1>Watch page</h1>
+    </div>
+  )
+}
+
+export default WatchPage;
