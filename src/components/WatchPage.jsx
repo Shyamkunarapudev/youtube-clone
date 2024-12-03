32a704385d7970715aa6e7a2da4e6f89778e3af0
shyamkunarapudev <shyamkumar14318@gmail.com>
2024-12-03 02:26:01 +0000

    I used useSearchParams from react-router-dom to find the params and there is a method known as useSearchParams.get('') pass inside variables which we want read in the url

--- src/components/WatchPage.jsx
+++ src/components/WatchPage.jsx
@@ -1,7 +1,18 @@
+import { useDispatch } from "react-redux";
+import { useSearchParams } from "react-router-dom";
+import { closeMenu } from "../utils/navSlice";
+import { useEffect } from "react";
+
+
 function WatchPage () {
+  const [searchParams] = useSearchParams()
+  const dispatch = useDispatch();
+  useEffect(() => {
+    dispatch(closeMenu())
+  }, []);
   return (
-    <div>
-      <h1>Watch page</h1>
+    <div className="mt-5 w-[90%] mx-auto">
+      <iframe className="rounded-xl" width="640" height="360" src={"https://www.youtube.com/embed/"+searchParams.get("v")} title="YouTube video player" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share" referrerPolicy="strict-origin-when-cross-origin" allowFullScreen></iframe>
     </div>
   )
 }
