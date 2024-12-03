52a1658cb66076dbf34c5a3ded3c81df43e8b885
shyamkunarapudev <shyamkumar14318@gmail.com>
2024-12-03 02:17:14 +0000

    Created link to every VideoCard component while maping in VideosContainerSection component

--- src/components/VideosContainerSection.jsx
+++ src/components/VideosContainerSection.jsx
@@ -1,6 +1,7 @@
 import { useEffect, useState } from "react";
 import YOUTUBE_VIDEOS_API from "../utils/constants";
 import VideoCard from "./VideoCard";
+import { Link } from "react-router-dom";
 
 function VideosContainerSection () {
   const [videos, setVideos] = useState([]);
@@ -15,7 +16,11 @@ function VideosContainerSection () {
   if(videos.length === 0) return;
   return (
     <div className="flex flex-wrap">
-      {videos.map(video => <VideoCard key={video.id} info={video}/>)}
+      {videos.map(video =>
+         <Link key={video.id} to={"/watch?v="+ video.id}>
+           <VideoCard info={video}/>
+         </Link>
+      )}
     </div>
   )
 }
